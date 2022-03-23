5b570147915a5d9671ef27a73eda76b797c3637c
aayush-55 <101275726+aayush-55@users.noreply.github.com>
2022-03-23 10:15:03 +0530

    Add files via upload

--- ad.java
+++ ad.java
@@ -1,80 +1,29 @@
-import  java.util.*;
+// top find no of object created  in a program 
+// using keyword static 
 
-class Bank 
+class Objcount 
 {
- int acc_no ;
- String name ;
- int amount ;
-  
-  void insert(int a ,String nm, int am)
+  static int i= 0 ;
+ 
+   Objcount()
+    {
+       i++ ;
+   }
+   
+   void display()
   {
-    acc_no= a ;
-    name=nm ;
-    amount=am ;
-    
-  }
-  
-  void deposit ( int d)
-  {
-    amount = amount + d;
-  } 
-  
-  void withdraw(int  w)
-  {
-    if(w<amount)
-    amount=amount-w ;
-    
-    else
-    System.out.println(" not suuficent balance");
-    
+    System.out.println(" NO OF OBJECTS CREATED " + i ) ;
 }
   
+
+public static void main(String args[])
+ {
+    Objcount  a = new Objcount() ;
+   Objcount  b= new Objcount() ;
+  Objcount  c= new Objcount() ;
+  Objcount  d  = new Objcount() ;
+  d.display() ;
+}
+
+}
   
-  void display()
-  {
-    System.out.println(acc_no + " " +name + " " + amount) ;
-    }
-    
-   
-   public static void main(String args [])
-   {
-    Scanner sc= new Scanner(System.in) ;
-    
-     System.out.println("enter account number") ;
-       int a =sc.nextInt() ;
-        sc.nextLine() ;
-      System.out.println("Enter your name") ;
-       String n=sc.nextLine() ;
-     
-     
-     
-       System.out.println("enter intial balance  ; ") ;
-        int bl =sc.nextInt() ;
-        
-        System.out.println("enter amount to deposit   ") ;
-        int d =sc.nextInt() ;
-        
-        System.out.println("enter amount to withdraw") ;
-        int w= sc.nextInt() ; 
-        
-        Bank b =new Bank() ;
-        b.insert(a,n,bl) ;
-        b.display() ;
-        b.deposit(d) ;
-        b.withdraw(w) ; 
-        b.display() ;
-        
-        } 
-        
-  }      
-        
-        
-        
-        
-        
-        
-        
-     
-     
-     
-   
